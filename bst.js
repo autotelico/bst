91ff22b8f206b31b55e01e72425cd04de52d5de0
Henrique Magalhães <henriqueheron1@gmail.com>
2025-04-14 19:49:21 -0300

    add insert method

--- bst.js
+++ bst.js
@@ -17,7 +17,7 @@ class Tree {
 
         const preparedArray = [...new Set(array)].sort((a, b) => a < b ? -1 : 1);
         console.log(`preparedArray: ${preparedArray}`);
-        
+
         const rootNodeIndex = Math.floor(preparedArray.length / 2);
         const rootNode = new Node(preparedArray[rootNodeIndex]);
 
@@ -28,6 +28,31 @@ class Tree {
         rootNode.right = this.buildTree(right)
 
         return rootNode;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        let tmp = this.root;
+
+        while (tmp !== null) {
+            if (tmp.value > value) {
+                // Go left
+                if (tmp.left === null) {
+                    tmp.left = newNode;
+                    return;
+                }
+                tmp = tmp.left;
+            } else {
+                // Go right
+                if (tmp.right === null) {
+                    tmp.right = newNode;
+                    return;
+                }
+                tmp = tmp.right;
+            }
+        }
+        
+
     }
 }
 
@@ -45,5 +70,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 const bt = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+bt.insert(6346)
 
 prettyPrint(bt.root)
